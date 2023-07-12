ed008984ad948fc4ef941d72805bf42b0af141df
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-07-12 17:13:56 +0200

    add component AppTitle from the beginning

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <script type="module">
-  import HelloWorld from './components/HelloWorld.vue'
+  import AppTitle from './components/AppTitle.vue';
   export default {
     components:{
-      HelloWorld
+      AppTitle
     }
   }
 </script>
 
 <template>
-  <HelloWorld />
+  <AppTitle/>
 </template>
 
 <style scoped>
---
+++ src/components/AppTitle.vue
@@ -0,0 +1,19 @@
+<script>
+  export default{
+    data(){
+      return {pageTitle: 'La mia prima app con Vite!'}
+    }
+  }
+</script>
+
+<template>
+  <h1>{{ pageTitle }}</h1>
+</template>
+
+<style>
+  h1{
+    text-align: center;
+    margin-top: 20px;
+    color: goldenrod;
+  }
+</style>
