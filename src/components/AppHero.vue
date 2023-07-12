e9b32f6b862a20227d02f9680aca5d35e7eb1c2f
Francescodc92 <francescodicorpo1992@hotmail.com>
2023-07-12 18:05:16 +0200

    add img component (end bonus)

---
+++ src/components/AppHero.vue
@@ -0,0 +1,25 @@
+<script>
+export default{
+  data(){
+    return {
+      srcImg : './src/assets/cane.jpg',
+    }
+  }
+}
+</script>
+
+<template>
+  <div class="img-wrapper">
+    <img :src="srcImg" alt="">
+  </div>
+</template>
+
+<style>
+  .img-wrapper{
+    margin: 0 auto;
+    max-width: 500px;
+  }
+  .img-wrapper img{
+    width: 100%;
+  }
+</style>
